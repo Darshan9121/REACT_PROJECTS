6b055e6b6a352566d5df634318ea8a310f383783
Darshan Hiragar <darshanhiragar44@gmail.com>
2024-09-30 21:50:21 +0530

    initial packages & routing done

--- bmi_calculator/src/App.js
+++ bmi_calculator/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Input from "./components/Input";
+import Result from "./components/Result";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route index element={<Home />} />
+    <Route path="/width" element={<Input/>}/>
+    <Route path="/height" element={<Input/>}/>
+    <Route path="/result" element={<Result />}/>   
+    </Routes>
+  </BrowserRouter>
   );
 }
 
